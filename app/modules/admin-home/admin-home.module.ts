92b893db68bcd6f5915ec3b11a1c9fff094356da
sait232 <85009810+sait232@users.noreply.github.com>
2021-10-12 11:18:44 +0530

    Add files via upload

---
+++ app/modules/admin-home/admin-home.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminHomeRoutingModule } from './admin-home-routing.module';
+import { AdminHomeComponent } from './admin-home.component';
+
+
+@NgModule({
+  declarations: [
+    AdminHomeComponent
+  ],
+  imports: [
+    CommonModule,
+    AdminHomeRoutingModule
+  ],
+  exports:[AdminHomeComponent]
+})
+export class AdminHomeModule { }
